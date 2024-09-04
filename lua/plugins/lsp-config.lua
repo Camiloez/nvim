af62326cdb4d0e512a9289193fc7a5697efb87cd
Camiloez <camilo.escobar.z@ug.uchile.cl>
2024-09-04 10:26:54 -0400

    chore: first commit with minimal working config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"mypy",
+					"ruff",
+					"pyright",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				filetypes = { "lua" },
+			})
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				filetypes = { "python" },
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
