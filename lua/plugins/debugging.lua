ece7963b121a18b87f8c9faad1dd8cb928ac620a
Camiloez <camilo.escobar.z@ug.uchile.cl>
2024-09-04 23:03:46 -0400

    fix: pluggins.debugging working with go

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,60 +1,31 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		--"nvim-neotest/nvim-nio",
-		--"leoluz/nvim-dap-go",
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap" },
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.after.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.after.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "leoluz/nvim-dap-go",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-		require("dapui").setup()
-		require("dap-go").setup()
+    require("dap-go").setup()
+    require("dapui").setup()
 
-		local path = "~/.pyenv/versions/debugpy/bin/python"
-		require("dap-python").setup(path)
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-
-		vim.keymap.set("n", "<Leader>dt", "<cmd> DapToggleBreakpoint <cr>", {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-	end,
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+  end,
 }
