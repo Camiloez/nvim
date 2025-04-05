fc34c12ca62cb3cd62c5474b58a9b490024fcde2
Camiloez <camilo.escobar.z@ug.uchile.cl>
2025-04-05 15:42:47 -0300

    chore: added go lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,23 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+			local util = require("lspconfig/util")
+
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true, 
+            }
+          }
+        }
+			})
 
 			vim.diagnostic.config({
 				signs = true,
