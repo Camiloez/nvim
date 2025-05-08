63fc83dc9f5b148bd405fe4d1f3e83d0abbfe133
Camilo <camilo.escobar.z@ug.uchile.cl>
2025-05-07 22:42:13 -0300

    chore: adding other keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,18 +6,18 @@ local keymaps = {
     ["<C-k>"] = { "<Up>", "Move up" },
   },
   n = {
-    ["<C-UP>"] = { "<cmd>resize -2<cr>", "Increase window height"},
+    ["<C-UP>"] = { "<cmd>resize -2<cr>", "Increase window height" },
+    ["<C-DOWN>"] = { "<cmd>resize +2<cr>", "Increase window height" },
     -- Splits
-    ["<leader>nh"] = {":nohl<CR>", "Clear search highlights"},
-    ["<leader>sv"] = {"<C-w>v", "split window vertically"},
-    ["<leader>sh"] = {"<C-w>s", "split horizontally"},
-    ["<leader>sx"] = {"<cmd>close<CR>", "close current split"},
+    ["<leader>nh"] = { ":nohl<CR>", "Clear search highlights" },
+    ["<leader>sv"] = { "<C-w>v", "split window vertically" },
+    ["<leader>sh"] = { "<C-w>s", "split horizontally" },
+    ["<leader>sx"] = { "<cmd>close<CR>", "close current split" },
     -- Tabs
-    ["<leader>to"] = {"<cmd>tabnew<CR>", "open new tab"},
-    ["<leader>tx"] = {"<cmd>tabclose<CR>", "close current tab"},
+    ["<leader>to"] = { "<cmd>tabnew<CR>", "open new tab" },
+    ["<leader>tx"] = { "<cmd>tabclose<CR>", "close current tab" },
   }
 }
-
 
 for mode, mappings in pairs(keymaps) do
   for key, mapping in pairs(mappings) do
@@ -26,3 +26,20 @@ for mode, mappings in pairs(keymaps) do
     vim.keymap.set(mode, key, action, { desc = desc })
   end
 end
+
+vim.keymap.set("n", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
+
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { noremap = true, silent = true })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('i', '<fn><bs>', '<C-w>')
+
+
+-- Indent text to the right using <Tab>
+vim.keymap.set("v", "<Tab>", ">gv", { silent = true })
+
+-- Indent text to the left using <S-Tab>
+vim.keymap.set("v", "<S-Tab>", "<gv", { silent = true })
