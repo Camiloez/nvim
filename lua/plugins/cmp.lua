56eb1209a6b7bc62a93ba3e88e763f17c1be6eb3
Camilo Escobar <camilo.escobar.z@ug.uchile.cl>
2025-05-23 11:17:05 -0400

    chore: remaped tab and s-tab to arrow up and down

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -126,8 +126,8 @@ return {
 				end,
 			},
 			mapping = {
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<Up>"] = cmp.mapping.select_prev_item(),
+				["<Down>"] = cmp.mapping.select_next_item(),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
