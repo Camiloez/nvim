dd3200ce6ebf9a1b6fa08ac6dfc78874bf7483c4
Camilo Escobar <camilo.escobar.z@ug.uchile.cl>
2025-05-26 00:23:07 -0400

    chore: remapped references lsp from <leader>gr to gr

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,13 +36,14 @@ return {
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<CR>", "o<Esc>")
 			vim.keymap.set("n", "<S-CR>", "O<Esc>")
 			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n" }, "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n"}, "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
 
 			--- Go
 			lspconfig.gopls.setup({
