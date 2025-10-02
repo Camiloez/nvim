01336820d243f39b4e9045b067ed755e61d4c0ec
caescobar_meli <camilo.escobar@mercadolibre.cl>
2025-10-02 13:00:08 -0300

    chore: trying to fix venv and pylsp

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,24 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	ft = { "lua", "python", "go" },
-	config = function()
-		local null_ls = require("null-ls")
-		local sources = {}
-
-		-- Always use stylua for lua
-		table.insert(sources, null_ls.builtins.formatting.stylua)
-
-		-- Only enable for Python when vim.g.pylsp is true
-		if vim.g.pylsp then
-			table.insert(sources, null_ls.builtins.formatting.black.with({ filetypes = { "python" } }))
-			table.insert(sources, null_ls.builtins.formatting.isort.with({ filetypes = { "python" } }))
-			table.insert(sources, null_ls.builtins.diagnostics.mypy.with({ filetypes = { "python" } }))
-		end
-
-		-- Always register go formatters, but restrict to go files
-		table.insert(sources, null_ls.builtins.formatting.gofumpt.with({ filetypes = { "go" } }))
-		table.insert(sources, null_ls.builtins.formatting.goimports.with({ filetypes = { "go" } }))
-
-		null_ls.setup({ sources = sources })
-	end,
+  "nvimtools/none-ls.nvim",
+  ft = { "lua", "python" },
+  config = function()
+    local null_ls = require "null-ls"
+    local sources = {}
+    if vim.g.pylsp then
+      sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.mypy,
+      }
+    end
+    table.insert(sources, null_ls.builtins.formatting.stylua)
+    null_ls.setup { sources = sources }
+  end,
 }
